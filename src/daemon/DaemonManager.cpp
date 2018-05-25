60a9d7c84e0836b89f905f2d49de2cf44c645b2a
s.skomudek <s.skomudek@devcups.com>
2018-05-25 19:10:27 +0100

    changed daemon name

--- src/daemon/DaemonManager.cpp
+++ src/daemon/DaemonManager.cpp
@@ -166,9 +166,9 @@ bool DaemonManager::stopWatcher(NetworkType::Type nettype) const
             if(counter >= 5) {
                 qDebug() << "Killing it! ";
 #ifdef Q_OS_WIN
-                QProcess::execute("taskkill /F /IM monerod.exe");
+                QProcess::execute("taskkill /F /IM bittubed.exe");
 #else
-                QProcess::execute("pkill monerod");
+                QProcess::execute("pkill bittubed");
 #endif
             }
 
@@ -300,9 +300,9 @@ DaemonManager::DaemonManager(QObject *parent)
 
     // Platform depetent path to monerod
 #ifdef Q_OS_WIN
-    m_monerod = QApplication::applicationDirPath() + "/monerod.exe";
+    m_monerod = QApplication::applicationDirPath() + "/bittubed.exe";
 #elif defined(Q_OS_UNIX)
-    m_monerod = QApplication::applicationDirPath() + "/monerod";
+    m_monerod = QApplication::applicationDirPath() + "/bittubed";
 #endif
 
     if (m_monerod.length() == 0) {
